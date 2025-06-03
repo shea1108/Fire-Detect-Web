7ba7c4a0f5eb68220fd506cb0e00c20ca3c0f811
Nguyễn Hoàng Thắng <shea11082003@gmail.com>
2025-06-04 02:02:40 +0700

    create and modify model files
    At deivces_model I added dev_hardware_id

--- backend/Models/logs_model.py
+++ backend/Models/logs_model.py
@@ -5,9 +5,9 @@ from backend.extensions import db
 
 class Log(db.Model):
     __tablename__ = 'logs'
-    log_id = db.Column(db.String(50), primary_key=True)
-    dev_id = db.Column(db.String(50), db.ForeignKey('devices.dev_id', ondelete="CASCADE", onupdate="CASCADE"))
-    model_id = db.Column(db.String(50), db.ForeignKey('models.model_id', ondelete="CASCADE", onupdate="CASCADE"))
+    log_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    dev_id = db.Column(db.Integer, db.ForeignKey('devices.dev_id', ondelete="CASCADE", onupdate="CASCADE"))
+    model_id = db.Column(db.Integer, db.ForeignKey('models.model_id', ondelete="CASCADE", onupdate="CASCADE"))
     log_fire_confidence = db.Column(db.Float, nullable=True)
     log_image_path = db.Column(db.String(255), nullable=True)
     log_create_at = db.Column(db.DateTime, default=datetime.utcnow)
---
+++ backend/Models/users_model.py
@@ -0,0 +1,15 @@
+from backend.extensions import db
+import uuid
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(100), nullable=False)
+    user_password = db.Column(db.String(255), nullable=False)
+    user_role = db.Column(db.String(20), nullable=False)
+    user_email = db.Column(db.String(100), unique=True, nullable=False)
+    user_phone_num = db.Column(db.String(10))
+    user_status = db.Column(db.Boolean, nullable=False, default=True)
+    user_create_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
