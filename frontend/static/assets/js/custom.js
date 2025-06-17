b2d89c050d2e5fdbde88d69d26fff819c1789479
phong2103470 <quocphong1147@gmail.com>
2025-06-17 23:27:00 +0700

    Update index, about

--- frontend/static/assets/js/custom.js
+++ frontend/static/assets/js/custom.js
@@ -1,58 +1,61 @@
-(function($) {
+/** @format */
+
+(function ($) {
     'use strict';
 
     // Mean Menu JS
-    jQuery('.mean-menu').meanmenu({ 
-        meanScreenWidth: "991"
+    jQuery('.mean-menu').meanmenu({
+        meanScreenWidth: '991',
     });
 
     // Navbar Area
-    $(window).on('scroll', function() {
-        if ($(this).scrollTop() >150){  
-            $('.navbar-area').addClass("sticky-nav");
-        }
-        else{
-            $('.navbar-area').removeClass("sticky-nav");
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar-area').addClass('sticky-nav');
+        } else {
+            $('.navbar-area').removeClass('sticky-nav');
         }
     });
 
     // FAQ Accordion JS
-	$('.accordion').find('.accordion-title').on('click', function(){
-		// Adds Active Class
-		$(this).toggleClass('active');
-		// Expand or Collapse This Panel
-		$(this).next().slideToggle('fast');
-		// Hide The Other Panels
-		$('.accordion-content').not($(this).next()).slideUp('fast');
-		// Removes Active Class From Other Titles
-		$('.accordion-title').not($(this)).removeClass('active');		
-	});
+    $('.accordion')
+        .find('.accordion-title')
+        .on('click', function () {
+            // Adds Active Class
+            $(this).toggleClass('active');
+            // Expand or Collapse This Panel
+            $(this).next().slideToggle('fast');
+            // Hide The Other Panels
+            $('.accordion-content').not($(this).next()).slideUp('fast');
+            // Removes Active Class From Other Titles
+            $('.accordion-title').not($(this)).removeClass('active');
+        });
 
-    // Brand Slider 
-     $('.brand-slider').owlCarousel({
+    // Brand Slider
+    $('.brand-slider').owlCarousel({
         loop: true,
         margin: 30,
         nav: false,
         dots: false,
         autoplay: true,
         autoplayHoverPause: true,
-        responsive:{
-            0:{
-                items: 2
+        responsive: {
+            0: {
+                items: 2,
             },
-            568:{
-                items: 3
+            568: {
+                items: 3,
             },
-            768:{
-                items: 5
+            768: {
+                items: 5,
             },
-            1000:{
-                items: 5
-            }
-        }
-    })
+            1000: {
+                items: 5,
+            },
+        },
+    });
 
-    // Portfolio Slider 
+    // Portfolio Slider
     $('.portfolio-slider').owlCarousel({
         loop: true,
         margin: 30,
@@ -62,22 +65,22 @@
         nav: true,
         navText: [
             "<i class='bx bx-left-arrow-alt'></i>",
-            "<i class='bx bx-right-arrow-alt'></i>"
+            "<i class='bx bx-right-arrow-alt'></i>",
         ],
-        responsive:{
-            0:{
-                items: 1
+        responsive: {
+            0: {
+                items: 1,
             },
-            768:{
-                items: 2
+            768: {
+                items: 2,
             },
-            1000:{
-                items: 3
-            }
-        }
-    })
+            1000: {
+                items: 3,
+            },
+        },
+    });
 
-    // Testimonial Slider 
+    // Testimonial Slider
     $('.testimonial-item-slider').owlCarousel({
         loop: true,
         items: 1,
@@ -87,11 +90,11 @@
         nav: true,
         navText: [
             "<i class='bx bx-left-arrow-alt'></i>",
-            "<i class='bx bx-right-arrow-alt'></i>"
+            "<i class='bx bx-right-arrow-alt'></i>",
         ],
-    })
+    });
 
-    // Service Slider 
+    // Service Slider
     $('.service-slider').owlCarousel({
         center: true,
         loop: true,
@@ -102,41 +105,43 @@
         nav: true,
         navText: [
             "<i class='bx bx-left-arrow-alt'></i>",
-            "<i class='bx bx-right-arrow-alt'></i>"
+            "<i class='bx bx-right-arrow-alt'></i>",
         ],
-        responsive:{
-            0:{
-                items: 1
+        responsive: {
+            0: {
+                items: 1,
             },
-            768:{
-                items: 2
+            768: {
+                items: 2,
             },
-            1000:{
-                items: 3
-            }
-        }
-    })
+            1000: {
+                items: 3,
+            },
+        },
+    });
 
     // Tabs
-    $('#tabs-item li a').on('click', function(e) {
-        $('#tabs-item li, #prices-content .active').removeClass('active').removeClass('fadeInUp');
+    $('#tabs-item li a').on('click', function (e) {
+        $('#tabs-item li, #prices-content .active')
+            .removeClass('active')
+            .removeClass('fadeInUp');
         $(this).parent().addClass('active');
         var activeTab = $(this).attr('href');
-        $( activeTab).addClass('active fadeInUp');
+        $(activeTab).addClass('active fadeInUp');
         e.preventDefault();
-    });	
+    });
 
-    // Popup Video 
+    // Popup Video
     $('.play-btn').magnificPopup({
         disableOn: 700,
         type: 'iframe',
         mainClass: 'mfp-fade',
         removalDelay: 160,
         preloader: false,
-        fixedContentPos: false
+        fixedContentPos: false,
     });
 
-    // Client Slider 
+    // Client Slider
     $('.client-slider').owlCarousel({
         center: true,
         loop: true,
@@ -147,91 +152,94 @@
         nav: true,
         navText: [
             "<i class='bx bx-left-arrow-alt'></i>",
-            "<i class='bx bx-right-arrow-alt'></i>"
+            "<i class='bx bx-right-arrow-alt'></i>",
         ],
-        responsive:{
-            0:{
-                items: 1
+        responsive: {
+            0: {
+                items: 1,
             },
-            768:{
-                items: 2
+            768: {
+                items: 2,
             },
-            1000:{
-                items: 3
-            }
-        }
-    })
+            1000: {
+                items: 3,
+            },
+        },
+    });
 
     // Search Botton
-    $('.close-btn').on('click',function() {
+    $('.close-btn').on('click', function () {
         $('.search-overlay').fadeOut();
         $('.search-btn').show();
         $('.close-btn').removeClass('active');
     });
-    $('.search-btn').on('click',function() {
+    $('.search-btn').on('click', function () {
         $(this).hide();
         $('.search-overlay').fadeIn();
         $('.close-btn').addClass('active');
     });
 
     // Subscribe form
-    $(".newsletter-form").validator().on("submit", function (event) {
-        if (event.isDefaultPrevented()) {
-            // Handle The Invalid Form...
-            formErrorSub();
-            submitMSGSub(false, "Please enter your email correctly");
-        } else {
-            // Everything Looks Good!
-            event.preventDefault();
-        }
-    });
-    function callbackFunction (resp) {
-        if (resp.result === "success") {
+    $('.newsletter-form')
+        .validator()
+        .on('submit', function (event) {
+            if (event.isDefaultPrevented()) {
+                // Handle The Invalid Form...
+                formErrorSub();
+                submitMSGSub(false, 'Please enter your email correctly');
+            } else {
+                // Everything Looks Good!
+                event.preventDefault();
+            }
+        });
+    function callbackFunction(resp) {
+        if (resp.result === 'success') {
             formSuccessSub();
-        }
-        else {
+        } else {
             formErrorSub();
         }
     }
-    function formSuccessSub(){
-        $(".newsletter-form")[0].reset();
-        submitMSGSub(true, "Thank you for subscribing!");
-        setTimeout(function() {
-            $("#validator-newsletter").addClass('hide');
-        }, 4000)
+    function formSuccessSub() {
+        $('.newsletter-form')[0].reset();
+        submitMSGSub(true, 'Thank you for subscribing!');
+        setTimeout(function () {
+            $('#validator-newsletter').addClass('hide');
+        }, 4000);
     }
-    function formErrorSub(){
-        $(".newsletter-form").addClass("animated shake");
-        setTimeout(function() {
-            $(".newsletter-form").removeClass("animated shake");
-        }, 1000)
+    function formErrorSub() {
+        $('.newsletter-form').addClass('animated shake');
+        setTimeout(function () {
+            $('.newsletter-form').removeClass('animated shake');
+        }, 1000);
     }
-    function submitMSGSub(valid, msg){
-        if(valid){
-            var msgClasses = "validation-success";
+    function submitMSGSub(valid, msg) {
+        if (valid) {
+            var msgClasses = 'validation-success';
         } else {
-            var msgClasses = "validation-danger";
+            var msgClasses = 'validation-danger';
         }
-        $("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
+        $('#validator-newsletter').removeClass().addClass(msgClasses).text(msg);
     }
-        
+
     // AJAX MailChimp
-    $(".newsletter-form").ajaxChimp({
-        url: "https://hibootstrap.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
-        callback: callbackFunction
+    $('.newsletter-form').ajaxChimp({
+        url: 'https://hibootstrap.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9', // Your url MailChimp
+        callback: callbackFunction,
     });
 
     // Back To Top Js
-    $('body').append('<div id="toTop" class="top-btn"><i class="bx bx-chevrons-up"></i></div>');
-    $(window).on('scroll',function () {
+    $('body').append(
+        '<div id="toTop" class="top-btn"><i class="bx bx-chevrons-up"></i></div>'
+    );
+    $(window).on('scroll', function () {
         if ($(this).scrollTop() != 0) {
             $('#toTop').fadeIn();
         } else {
             $('#toTop').fadeOut();
         }
-    }); 
-    $('#toTop').on('click',function(){
-        $("html, body").animate({ scrollTop: 0 }, 0);
+    });
+    $('#toTop').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 0);
         return false;
     });
 
@@ -239,14 +247,14 @@
     new WOW().init();
 
     // Preloader JS
-    jQuery(window).on('load',function(){
-        jQuery(".preloader").fadeOut(500);
+    jQuery(window).on('load', function () {
+        jQuery('.preloader').fadeOut(500);
     });
 
     // Switch Btn
-    $('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
-
-
+    $('body').append(
+        "<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>"
+    );
 })(jQuery);
 
 // function to set a given theme/color-scheme
@@ -271,8 +279,6 @@ function toggleTheme() {
         document.getElementById('slider').checked = false;
     } else {
         setTheme('theme-light');
-      document.getElementById('slider').checked = true;
+        document.getElementById('slider').checked = true;
     }
 })();
-
-
