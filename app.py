c7c513dcb1a395efa20bf5285f37af1498336366
itdevphat <nguyenthanhphat.work@gmail.com>
2025-06-27 13:46:06 +0700

    fix giao dien + mat khau khi tao 8 ky tu (in hoa, thuong, so, ki tu dac biet)

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 import os
-from dotenv import load_dotenv, dotenv_values
+
 from backend import create_app, socketio
-import ssl
+
 import logging
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile='certs/cert.pem', keyfile='certs/key.pem')
-load_dotenv()  # Nạp biến môi trường từ file .env
+
+
 # Ẩn lỗi do client ngắt video sớm
 logging.getLogger("eventlet.wsgi.server").setLevel(logging.ERROR)
 
@@ -22,10 +21,20 @@ logging.getLogger("eventlet.wsgi.server").setLevel(logging.ERROR)
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, use_reloader=True, host='127.0.0.1', port=5000)
-   
+    # socketio.run(app, debug=True, use_reloader=True, host='127.0.0.1', port=5000)
+    # socketio.run(app, debug=True, use_reloader=True, host='192.168.10.8', port=5000, ssl_context=context)
+    host = '192.168.10.8' # 127.0.0.1
+    port = 5000
+    use_ssl = True  # đổi thành False nếu không dùng SSL
 
-    # socketio.run(app, debug=True, use_reloader=True, host='192.168.10.11', port=5000, ssl_context=context)
-    
+    if use_ssl:
+        import ssl
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain(certfile='certs/cert.pem', keyfile='certs/key.pem')
+        print(f'🚀 FireDetect is running with HTTPS at https://{host}:{port}')
+        socketio.run(app, debug=True, use_reloader=True, host=host, port=port, ssl_context=context)
+    else:
+        print(f'🚀 FireDetect is running at http://{host}:{port}')
+        socketio.run(app, debug=True, use_reloader=True, host=host, port=port)
 
 
